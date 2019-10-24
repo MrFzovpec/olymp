29379461fae61836e9161463c02cbc3fa62c346b
Fzovpec <obgpsuper@gmail.com>
2019-10-24 16:20:36 +0300

    check

--- 2.py
+++ 2.py
@@ -1,13 +1,35 @@
-a = int(input())
-b = int(input())
+# a = int(input())
+# b = int(input())
+#
+# ch = 0
+# nch = 0
+#
+# for i in range(a, b + 1):
+#     if i % 2 == 0:
+#         ch += i
+#     else:
+#         nch += i
+#
+# print(ch - nch)
 
+
+a = int(input("Введите А = "))
+b = int(input("Введите B = "))
 ch = 0
-nch = 0
-
-for i in range(a, b + 1):
-    if i % 2 == 0:
-        ch += i
+nech = 0
+n=a
+while (n>=a)and(n<=b)and(a<=100)and(b<=100):
+    if n%2 == 0:
+        ch = ch + n
+        n=n+1
     else:
-        nch += i
+        nech = nech + n
+        n=n+1
+else:
+    print("Введите другие числа")
 
-print(ch - nch)
+print("Сумма четных = " + str(ch))
+print("Сумма нечетных = " + str(nech))
+raz=ch-nech
+
+print(raz)
