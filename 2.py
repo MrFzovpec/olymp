b91524e7b8a24869cc6eb3ea75419786084c5d31
Fzovpec <obgpsuper@gmail.com>
2019-10-24 16:26:36 +0300

    check

--- 2.py
+++ 2.py
@@ -1,35 +1,17 @@
-# a = int(input())
-# b = int(input())
-#
-# ch = 0
-# nch = 0
-#
-# for i in range(a, b + 1):
-#     if i % 2 == 0:
-#         ch += i
-#     else:
-#         nch += i
-#
-# print(ch - nch)
-
-
-a = int(input("Введите А = "))
-b = int(input("Введите B = "))
+a = int(input())
+b = int(input())
 ch = 0
 nech = 0
-n=a
-while (n>=a)and(n<=b)and(a<=100)and(b<=100):
-    if n%2 == 0:
+n = a
+while (n >= a)and(n <= b)and(a <= 100)and(b <= 100):
+    if n % 2 == 0:
         ch = ch + n
-        n=n+1
+        n = n + 1
     else:
         nech = nech + n
-        n=n+1
-else:
-    print("Введите другие числа")
+        n = n + 1
 
-print("Сумма четных = " + str(ch))
-print("Сумма нечетных = " + str(nech))
-raz=ch-nech
+
+raz = ch - nech
 
 print(raz)
--- 3.py
+++ 3.py
@@ -1,4 +1,12 @@
-a = int(input())
-b = int(input())
+# a = int(input())
+# b = int(input())
+#
+# print(a * (b - 1) + 1)
 
-print(a * (b - 1) + 1)
+a, b = int(input()), int(input())
+n = 1
+
+while not(((n - 1) % a == 0) and ((n + 1) % b == 0)):
+    n += 1
+
+print(n)
